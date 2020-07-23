16d980efab65211d31517e99732c5a3040cb2908
Eric Ferguson <ericferguson@gmail.com>
2020-07-23 15:42:48 -0500

    Adding a few more test cases

--- C++/Foundation Classes/testcases/testcase_01.cpp
+++ C++/Foundation Classes/testcases/testcase_01.cpp
@@ -6,10 +6,10 @@
 class A : public FCRetainable
 {
 public:
-    A() : id_(id++) { printf("Creating %u\n", getId()) ; }
-    ~A()            { printf("Deleting %u\n", getId()) ; }
+    A() : id_(id++) { printf("\tCreating %u\n", getId()) ; }
+    ~A()            { printf("\tDeleting %u\n", getId()) ; }
 
-    void print() { printf("  This is object A(%u)\n", getId()) ; }
+    void print() { printf("\tThis is object A(%u)\n", getId()) ; }
 
 private:
     unsigned int id_ ;
@@ -21,60 +21,57 @@ private:
 unsigned int A::id ;
 
 void somefunc_01( A*   p ) { ; }
-void somefunc_02( P<A> p ) { ; }
+void somefunc_02( P<A> p ) { p->print() ; }
 
 P<A> somefunc_03() { P<A> p ; p.NEW(new A()); return p; }
 P<A> somefunc_04(P<A> p) { return p; }
 
 int main( int ac, char **av )
 {
+    printf("Running A\n") ;
     {
         P<A> a ;
         a.NEW(new A()) ;
         a->print() ;
     }
 
-    printf("\n") ;
-
+    printf("  done w/ A\n") ;
+    printf("Running B\n") ;
     {
         P<A> a(new A());
         a->print() ;
     }
 
-    printf("\n") ;
-
+    printf("  done w/ B\n") ;
+    printf("Running C\n") ;
     {
         P<A> a = new A() ;
         a->print() ;
     }
 
-    printf("\n") ;
-
+    printf("  done w/ C\n") ;
+    printf("Running D\n") ;
     {
         P<A> a, b ;
         a.NEW( new A() );
         a->print() ;
     }
 
-    printf("\n") ;
-
+    printf("  done w/ D\n") ;
+    printf("Running E\n") ;
     {
         P<A> a ;
         a.NEW( new A() );
         (*a).print() ;
-    }
-
-    printf("\n") ;
-
+    } printf("  done w/ E\n") ;
+    printf("Running F\n") ;
     {
         P<A> a ;
         a.NEW( new A() );
         somefunc_01( a ) ;
-        a->print() ;
-    }
-
-    printf("\n") ;
-
+        a->print() ; } 
+    printf("  done w/ F\n") ;
+    printf("Running G\n") ;
     {
         P<A> a ;
         a.NEW( new A() );
@@ -82,8 +79,8 @@ int main( int ac, char **av )
         a->print() ;
     }
 
-    printf("\n") ;
-
+    printf("  done w/ G\n") ;
+    printf("Running H\n") ;
     {
         P<A> a, b ;
         a.NEW(new A()) ;
@@ -93,18 +90,31 @@ int main( int ac, char **av )
         b->print() ;
     }
 
-    printf("\n") ;
-
+    printf("  done w/ H\n") ;
+    printf("Running I\n") ;
     {
         P<A> a = somefunc_03() ;
         a->print() ;
     }
 
-    printf("\n") ;
-
+    printf("  done w/ I\n") ;
+    printf("Running J\n") ;
+    {
+        P<A> a ;
+        a.NEW(new A()) ;
+        P<A> b = somefunc_04(a) ;
+    }
+    printf("  done w/ J\n") ;
+    printf("Running K\n") ;
     {
         P<A> a = somefunc_04(somefunc_03()) ;
         a->print() ;
     }
-
+    printf("  done w/ K\n") ;
+    printf("Running L\n") ;
+    {
+        P<A> a = somefunc_04(somefunc_04(somefunc_03())) ;
+        a->print() ;
+    }
+    printf("  done w/ L\n") ;
 }
